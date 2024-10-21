a8bf63086fff6b72d3044bf53c924a01c8a2d69c
Sebas Morante <sebasmorante@iMac-de-Sebas.local>
2024-10-21 09:06:38 +0200

    ejemplo transaciones

---
+++ upcoin-project/scripts/deploy-sp2.js
@@ -0,0 +1,383 @@
+require("dotenv").config({ path: '../.env' });;
+const {Web3} = require('web3');
+
+/*deploy-sp2: Ejemplo de como se debera de implementar las transaciones entre cuentas*/ 
+
+console.log('PRIVATE_KEY:', process.env.PRIVATE_KEY);
+console.log('INFURA_API_URL:', process.env.INFURA_API_URL);
+
+// Conectar al nodo de la testnet Sepolia usando el enlace de Infura desde el archivo .env
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_API_URL));
+
+// Obtener la clave privada de las variables de entorno
+const privateKey = `0x${process.env.PRIVATE_KEY.trim()}`;
+const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+web3.eth.accounts.wallet.add(account);
+
+// La dirección del contrato de tu criptomoneda UPCoin
+const contractAddress = "0x3878ecED3ad9a37dD8f3D620d7A0b62f1a90535B";
+
+// El ABI de tu contrato UPCoin (obténlo al compilar el contrato)
+const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "initialSupply",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "allowance",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "needed",
+          "type": "uint256"
+        }
+      ],
+      "name": "ERC20InsufficientAllowance",
+      "type": "error"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "sender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "balance",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "needed",
+          "type": "uint256"
+        }
+      ],
+      "name": "ERC20InsufficientBalance",
+      "type": "error"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "approver",
+          "type": "address"
+        }
+      ],
+      "name": "ERC20InvalidApprover",
+      "type": "error"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "receiver",
+          "type": "address"
+        }
+      ],
+      "name": "ERC20InvalidReceiver",
+      "type": "error"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "sender",
+          "type": "address"
+        }
+      ],
+      "name": "ERC20InvalidSender",
+      "type": "error"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        }
+      ],
+      "name": "ERC20InvalidSpender",
+      "type": "error"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
+
+// Crear una instancia del contrato
+const contract = new web3.eth.Contract(abi, contractAddress);
+
+// Función para transferir UPCoin
+async function transferUPCoin(toAddress, amount) {
+  const decimals = 18;  // Nuestro token es de 18 decimales
+
+  // Convertir el monto a la cantidad correcta con decimales
+  // TODO: Acabar de mirar por que toBN nomlo reconoce...
+  //const tokenAmount = web3.utils.toBN(amount * 10 ** decimals); 
+  const tokenAmount = web3.utils.toWei(amount, "ether"); // Convierte a Wei
+
+  // Crear los datos de la transacción (llamando a la función "transfer" del contrato ERC-20)
+  const tx = contract.methods.transfer(toAddress, tokenAmount);
+
+  // Obtener el gas estimado para la transacción
+  const gas = await tx.estimateGas({ from: account.address });
+
+  // Obtener el gas price actual de la red
+  const gasPrice = await web3.eth.getGasPrice();
+
+  // Construir la transacción
+  const txData = {
+    from: account.address,
+    to: contractAddress,
+    data: tx.encodeABI(),
+    gas,
+    gasPrice
+  };
+
+  // Firmar la transacción con la clave privada
+  const signedTx = await web3.eth.accounts.signTransaction(txData, privateKey);
+
+  // Enviar la transacción firmada
+  const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  console.log('Transacción enviada: ', receipt.transactionHash);
+  console.log('Transferencia exitosa');
+}
+
+// Ejemplo de transferencia de 10 UPCoin a la walled con direccion 0x2b4...
+transferUPCoin("0x2b41659B028269Fe71E6683c7240294cdD9607e1", 10);
