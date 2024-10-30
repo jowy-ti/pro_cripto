9f2ce86c80b2d15eb15569cf96e2b0408f447ba6
xavier.lopez.manes <xavier.lopez.manes@estudiantat.upc.edu>
2024-10-30 17:46:18 +0100

    new backend

--- codigo-backend/server.js
+++ codigo-backend/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
 const crypto = require('crypto');
+
 const app = express();
+app.use(bodyParser.json());
 const cookieParser = require('cookie-parser');
 const mongo_functions = require("./MongoDB.js");
-const { mongo } = require("mongoose");
+const {Web3} = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_API_URL));
 
+
+/*app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));*/
 app.use(express.json());
 app.use(cookieParser());
 app.use(session({
@@ -79,6 +90,11 @@ app.listen(5000, () => {
     console.log("Server started on port 5000");
 });
 
+app.get("/", async(req, res)  => {
+    const products = await mongo_functions.getAllProducts();
+    res.json(products)
+});
+
 (async () => {
     try {
         await mongo_functions.connectToDatabase();
@@ -97,5 +113,132 @@ app.get("/dashboard", (req,res) =>{
     }
     else {
         console.log("No SESSION ID")
+    }
+});
+
+
+
+
+// Dirección del contrato del Relayer
+const relayerAddress = '0x4cB654441C5971b73179DDe42F02f26E7cf3e287';
+const relayerABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_upcoinAddress",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bytes",
+          "name": "signature",
+          "type": "bytes"
+        }
+      ],
+      "name": "relayTransfer",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "upcoin",
+      "outputs": [
+        {
+          "internalType": "contract UPCoin",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+];
+const relayerContract = new web3.eth.Contract(relayerABI, relayerAddress);
+
+// Endpoint para realizar la transferencia
+app.post('/relay-transfer', async (req, res) => {
+    const { from, to, amount, signature } = req.body;
+    console.log("Datos recibidos:", req.body);
+
+    try {
+        console.log("Validando firma...");
+        const gasPrice = await web3.eth.getGasPrice();
+        console.log("Gas Price obtenido:", gasPrice);
+
+        const gasEstimate = await relayerContract.methods.relayTransfer(from, to, amount, signature).estimateGas({ from });
+        console.log("Estimación de Gas:", gasEstimate);
+
+        const nonce = await web3.eth.getTransactionCount(process.env.RELAYER_ADDRESS);
+
+        const txData = {
+            to: relayerAddress,
+            data: relayerContract.methods.relayTransfer(from, to, amount, signature).encodeABI(),
+            gas: gasEstimate,
+            gasPrice,
+            nonce,
+            //from: from,
+            from: process.env.RELAYER_ADDRESS,
+        };
+
+        // Firmar la transacción
+        const signedTx = await web3.eth.accounts.signTransaction(txData, process.env.PRIVATE_KEY); // original
+
+
+        // Enviar la transacción firmada
+        const tx = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+        console.log("Transacción:", tx);
+        console.log("FIN TRANSACCIÓN");
+
+        //res.json({ message: 'Transferencia realizada', tx });
+        res.json({
+            message: 'Transferencia realizada',
+            tx: {
+                blockHash: tx.blockHash,
+                blockNumber: tx.blockNumber.toString(), // Convertir blockNumber a cadena
+                cumulativeGasUsed: tx.cumulativeGasUsed.toString(), // Convertir cumulativeGasUsed a cadena
+                effectiveGasPrice: tx.effectiveGasPrice.toString(), // Convertir effectiveGasPrice a cadena
+                from: tx.from,
+                gasUsed: tx.gasUsed.toString(), // Convertir gasUsed a cadena
+                logs: tx.logs.map(log => ({
+                    ...log,
+                    blockNumber: log.blockNumber.toString(), // Convertir blockNumber en logs a cadena
+                    logIndex: log.logIndex.toString(), // Convertir logIndex a cadena
+                    transactionIndex: log.transactionIndex.toString(), // Convertir transactionIndex a cadena
+                })),
+                logsBloom: tx.logsBloom,
+                status: tx.status.toString(), // Convertir status a cadena
+                to: tx.to,
+                transactionHash: tx.transactionHash,
+                transactionIndex: tx.transactionIndex.toString(), // Convertir transactionIndex a cadena
+                type: tx.type.toString(), // Convertir type a cadena
+            },
+        });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error en la transferencia' });
     }
 });
