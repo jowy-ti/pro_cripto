73867578a69e986e350f03bc97261a05938a675d
Sebas Morante <sebas@MacBook-Pro-de-Sebas.local>
2024-12-04 14:05:18 +0100

    Nueva HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import Menu from '../components/Menu';  // Importa el componente del menú
@@ -9,34 +10,35 @@ const HomePage = () => {
 
   return (
     <div className="homepage-container">
-
-    {/* Cabecera de página */}
-    <header className="page-header">
-        <h1 className="page-header-title">
-          <span className="logo"></span>
-          UPCoin
-          <span className="separator">|</span>
-          <span className="subtitle">Página de Inicio</span>
-        </h1>
-        {/* Usar el componente Menu */}
-          <Menu />
+      {/* Cabecera de página */}
+      <header className="page-header">
+          <h1 className="page-header-title">
+            <span className="logo"></span>
+            UPCoin
+            <span className="separator">|</span>
+            <span className="subtitle">Página de Inicio</span>
+          </h1>
+          {/* Usar el componente Menu */}
+            <Menu />
       </header>
 
-      {/* Imagen con texto superpuesto */}
+      {/* Imagen de cabecera */}
       <div className="header-image-hp">
-        <h1 className="header-title">UPCoin</h1>
-        <p className="intro-subtitle-hp">Plataforma de comercio</p>
-        <p className="header-slogan">La plataforma de comercio creada para UPCoin</p>
-
-        {/* Contenedor de los botones dentro de la imagen */}
-        <div className="button-container-home">
-          <button className="nav-button-home" onClick={() => navigate("/shoppage")}>
-            Ir a la plataforma
-          </button>
+        {/* Mensaje de bienvenida */}
+        <span className="logo-hp"></span>
+        <div className="intro-text-hp">
+          <p className="intro-title-hp">Hola UPCoin</p>
+          <p className="intro-subtitle-hp">La Nueva Criptomoneda de la UPC</p>
+          <p className="intro-description-hp">
+            UPCoin es una criptomoneda basada en el estándar ERC20, diseñada para transformar la economía de la UPC. 
+            Con un contrato inteligente desplegado en la testnet de Sepolia, UPCoin permite a estudiantes, profesores 
+            y personal realizar pagos y transacciones de forma rápida, segura y eficiente dentro del entorno académico. 
+            Únete a la revolución digital y empieza a disfrutar de los beneficios de UPCoin.
+          </p>
         </div>
       </div>
 
-      {/* Nueva sección para la configuración del usuario */}
+      {/* Nueva sección para la configuración del usuario*/}
       <div className="user-setup">
         <h2 className="setup-title">¿Aún no has empezado a utilizar UPCoin?</h2>
         <p className="setup-description">
@@ -45,6 +47,26 @@ const HomePage = () => {
         <div className="button-container-setup">
           <button className="nav-button-setup" onClick={() => window.location.href = "/getstartedpage"}>
             Empezar a utilizar UPCoin
+          </button>
+        </div>
+      </div>
+
+      {/* Imagen de plataforma de comercio*/}
+      <div className="image-hp-pc">
+        {/* Mensaje de bienvenida */}
+        <div className="pc-text-hp">
+          <p className="pc-title-hp">Plataforma de comercio UPCoin</p>
+          <p className="pc-subtitle-hp">Un marketplace único para la comunidad UPC</p>
+          <p className="pc-description-hp">
+          Explora una amplia variedad de productos y servicios ofrecidos por diferentes comercios. 
+          Realiza tus compras de forma segura y rápida utilizando UPCoin, la criptomoneda que impulsa nuestra economía.
+          </p>
+        </div>
+
+        {/* Contenedor de los botones dentro de la imagen*/}
+        <div className="button-container-pc">
+          <button className="nav-button-pc" onClick={() => navigate("/shoppage")}>
+            Ir a la plataforma de comercio
           </button>
         </div>
       </div>
@@ -69,6 +91,6 @@ const HomePage = () => {
       </footer>
     </div>
   );
-};
 
+};
 export default HomePage;
